58a427fd6bf2d52fadadb5ca2cab28e846cf3711
Yunke Qu <37522093+qykcq@users.noreply.github.com>
2026-01-30 14:43:49 +0800

    Update eval_util.py

--- util/eval_util.py
+++ util/eval_util.py
@@ -39,61 +39,61 @@ def process_ranking_metrics(recalls_5, recalls_10, recalls_20, ndcgs_5, ndcgs_10
     return avg
 
 
-def eval_rec(recsys, dataset, user_sample_ratio):
-    t1 = time.time()
-    sampled_users = np.random.choice(
-        dataset.user_vocab,
-        round(user_sample_ratio * dataset.n_users),
-        replace=False
-    )
-    # sampled_items = np.random.choice(
-    #     dataset.item_vocab,
-    #     round(1 * dataset.n_items),
-    #     replace=False
-    # )
-    sampled_items = np.array(dataset.item_vocab)
-    recalls_20, ndcgs_20, recalls_10, ndcgs_10, recalls_5, ndcgs_5 = [], [], [], [], [], []
+# def eval_rec(recsys, dataset, user_sample_ratio):
+#     t1 = time.time()
+#     sampled_users = np.random.choice(
+#         dataset.user_vocab,
+#         round(user_sample_ratio * dataset.n_users),
+#         replace=False
+#     )
+#     # sampled_items = np.random.choice(
+#     #     dataset.item_vocab,
+#     #     round(1 * dataset.n_items),
+#     #     replace=False
+#     # )
+#     sampled_items = np.array(dataset.item_vocab)
+#     recalls_20, ndcgs_20, recalls_10, ndcgs_10, recalls_5, ndcgs_5 = [], [], [], [], [], []
 
-    chunk_size = math.ceil(len(sampled_users) / 1)
-    for chunk in range(1):
-        start_ind = chunk * chunk_size
-        end_ind = min(len(sampled_users), (chunk + 1) * chunk_size)
-        users_in_chunk = sampled_users[start_ind: end_ind]
+#     chunk_size = math.ceil(len(sampled_users) / 1)
+#     for chunk in range(1):
+#         start_ind = chunk * chunk_size
+#         end_ind = min(len(sampled_users), (chunk + 1) * chunk_size)
+#         users_in_chunk = sampled_users[start_ind: end_ind]
 
-        y_pred, topk_ind = get_y_pred(recsys, users_in_chunk, sampled_items, dataset)
+#         y_pred, topk_ind = get_y_pred(recsys, users_in_chunk, sampled_items, dataset)
 
-        assert len(y_pred) == len(users_in_chunk)
+#         assert len(y_pred) == len(users_in_chunk)
 
-        for user_id in users_in_chunk:
-            assert dataset.user_vocab[user_id] == user_id
-            total = np.sum(dataset.get_y_true_by_user(user_id)[sampled_items])
-            # the position of user_id
-            user_pos = np.asarray(users_in_chunk == user_id).nonzero()
-            assert len(user_pos[0]) == 1
-            user_pos = user_pos[0][0]
-            assert dataset.get_y_true_by_user(user_id)[sampled_items].shape == y_pred[user_pos].shape
+#         for user_id in users_in_chunk:
+#             assert dataset.user_vocab[user_id] == user_id
+#             total = np.sum(dataset.get_y_true_by_user(user_id)[sampled_items])
+#             # the position of user_id
+#             user_pos = np.asarray(users_in_chunk == user_id).nonzero()
+#             assert len(user_pos[0]) == 1
+#             user_pos = user_pos[0][0]
+#             assert dataset.get_y_true_by_user(user_id)[sampled_items].shape == y_pred[user_pos].shape
 
-            # dataset.get_y_true_by_user(user_id) is only one single row
-            y_true_selected = dataset.get_y_true_by_user(user_id)[sampled_items]
-            # y_pred_selected is only a single row
-            y_pred_selected = y_pred[user_pos]
-            # topk indices associated with this specific user
-            selected_topk_ind = topk_ind[user_pos]
-            zipped = list(zip(y_true_selected[selected_topk_ind], y_pred_selected[selected_topk_ind]))
+#             # dataset.get_y_true_by_user(user_id) is only one single row
+#             y_true_selected = dataset.get_y_true_by_user(user_id)[sampled_items]
+#             # y_pred_selected is only a single row
+#             y_pred_selected = y_pred[user_pos]
+#             # topk indices associated with this specific user
+#             selected_topk_ind = topk_ind[user_pos]
+#             zipped = list(zip(y_true_selected[selected_topk_ind], y_pred_selected[selected_topk_ind]))
 
-            recalls_20.append(recall_at_k(zipped, total, k=20))
-            recalls_10.append(recall_at_k(zipped, total, k=10))
-            recalls_5.append(recall_at_k(zipped, total, k=5))
+#             recalls_20.append(recall_at_k(zipped, total, k=20))
+#             recalls_10.append(recall_at_k(zipped, total, k=10))
+#             recalls_5.append(recall_at_k(zipped, total, k=5))
 
-            ideal_rank = np.sort(dataset.get_y_true_by_user(user_id)[sampled_items])[::-1]
+#             ideal_rank = np.sort(dataset.get_y_true_by_user(user_id)[sampled_items])[::-1]
 
-            ndcgs_20.append(ndcg_at_k(zipped, ideal_rank, k=20))
-            ndcgs_10.append(ndcg_at_k(zipped, ideal_rank, k=10))
-            ndcgs_5.append(ndcg_at_k(zipped, ideal_rank, k=5))
-    print('Time used: {:.2f}'.format(time.time() - t1))
-    return process_ranking_metrics(recalls_5, recalls_10, recalls_20, ndcgs_5, ndcgs_10, ndcgs_20)
+#             ndcgs_20.append(ndcg_at_k(zipped, ideal_rank, k=20))
+#             ndcgs_10.append(ndcg_at_k(zipped, ideal_rank, k=10))
+#             ndcgs_5.append(ndcg_at_k(zipped, ideal_rank, k=5))
+#     print('Time used: {:.2f}'.format(time.time() - t1))
+#     return process_ranking_metrics(recalls_5, recalls_10, recalls_20, ndcgs_5, ndcgs_10, ndcgs_20)
 
-def eval_rec_fast(recsys, dataset, user_sample_ratio, ks=(5, 10, 20)):
+def eval_rec(recsys, dataset, user_sample_ratio, ks=(5, 10, 20)):
     t1 = time.time()
 
     sampled_users = np.random.choice(

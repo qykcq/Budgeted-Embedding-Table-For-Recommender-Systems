a0b8d586480ca0e231e8235e4948c314246bcc66
Yunke Qu <37522093+qykcq@users.noreply.github.com>
2026-01-31 21:16:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from module import config
+from util.train_util import initialize_dataset, train_till_convergence
+from util.eval_util import eval_rec
+from base_recsys.LightGCN import LightGCN
+
 
 dataset = initialize_dataset(with_validation=True)
 LightGCN(dataset, user_sizes, item_sizes).to(config.device)

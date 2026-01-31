83d608617c711991ef4a47e72f6ee9bbd36f5c40
Yunke Qu <37522093+qykcq@users.noreply.github.com>
2026-01-31 21:23:15 +0800

    Update LightGCN.py

--- base_recsys/LightGCN.py
+++ base_recsys/LightGCN.py
@@ -14,8 +14,8 @@ class LightGCN(nn.Module):
         self.num_items = self.dataset.n_items
 
         if user_sizes is None:
-            user_sizes = np.ones(self.num_users) * MAX_EMB_SIZE
-            item_sizes = np.ones(self.num_items) * MAX_EMB_SIZE
+            user_sizes = np.ones(self.num_users) * config.MAX_EMB_SIZE
+            item_sizes = np.ones(self.num_items) * config.MAX_EMB_SIZE
 
         self.max_emb = max(max(user_sizes), max(item_sizes))
 

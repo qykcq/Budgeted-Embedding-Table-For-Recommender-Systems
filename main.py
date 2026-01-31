398a9478255bfc4cca98d88db11d7edfdcbf0e3a
Yunke Qu <37522093+qykcq@users.noreply.github.com>
2026-01-31 21:55:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from util.train_util import initialize_dataset, train_till_convergence
 from util.eval_util import eval_rec
 from base_recsys.LightGCN import LightGCN
 
-
-config.DATASET_NAME = 'yelp'
-config.BASE_MODEL = 'lightgcn'
-dataset = initialize_dataset(with_validation=True)
-recsys = LightGCN(dataset).to(config.device)
-eval_rec(recsys, dataset, 1)
+for ds in ['yelp', 'gowalla']:
+  config.DATASET_NAME = 'yelp'
+  config.BASE_MODEL = 'lightgcn'
+  dataset = initialize_dataset(with_validation=True)
+  recsys = LightGCN(dataset).to(config.device)
+  eval_rec(recsys, dataset, 1)

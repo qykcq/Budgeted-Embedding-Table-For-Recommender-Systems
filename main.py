688f9119627232a0d9f6013894f7d4620a75a5f5
Yunke Qu <37522093+qykcq@users.noreply.github.com>
2026-01-31 21:18:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from util.train_util import initialize_dataset, train_till_convergence
 from util.eval_util import eval_rec
 from base_recsys.LightGCN import LightGCN
 
-
+config.DATASET_NAME = 'gowalla'
 dataset = initialize_dataset(with_validation=True)
 LightGCN(dataset, user_sizes, item_sizes).to(config.device)
 eval_rec(recsys, dataset, 1)

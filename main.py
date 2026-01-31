9fa0ac4e61af9154bee9e8f5735c33fbd547c09a
Yunke Qu <37522093+qykcq@users.noreply.github.com>
2026-01-31 21:33:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from util.eval_util import eval_rec
 from base_recsys.LightGCN import LightGCN
 
 config.DATASET_NAME = 'gowalla'
-
+config.BASE_MODEL = 'lightgcn'
 dataset = initialize_dataset(with_validation=True)
 LightGCN(dataset).to(config.device)
 eval_rec(recsys, dataset, 1)
